5c1898207f86e4ba2c6636025f68181a6c71282f
Pavel Zagumenkin <pavelzagumenkin@gmail.com>
2022-07-04 19:36:37 +0400

    Перечисленно 10 типов данных

---
+++ python_type_dates.py
@@ -0,0 +1,29 @@
+a = None #Отсутствие данных
+print(type(a))
+
+a = 1 #Целое число
+print(type(a))
+
+a = 1.0 #Число с плавающей точкой
+print(type(a))
+
+a = 1 + 1j #Комплексное число
+print(type(a))
+
+a = '1' #Строчное значение
+print(type(a)) 
+
+a = [1, 1, 'a'] #Список
+print(type(a))
+
+a = (1, 1, 'a') #Кортеж
+print(type(a))
+
+a = {1, 2, 'a'} #Множество
+print(type(a))
+
+a = {'a':1, 'b':2} #Словарь
+print(type(a))
+
+a = True #Булевое значение может быть либо True(1) либо False(0)
+print(type(a))
